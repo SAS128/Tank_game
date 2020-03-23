751d90867f9255d3988dd43495f610e8adef2ebc
Александр Захаров <alexz20937cany3@gmail.com>
2020-03-23 20:53:59 +0200

    Проверки+контуры

--- TankGame/TankGame/Form1.Designer.cs
+++ TankGame/TankGame/Form1.Designer.cs
@@ -1,16 +1,21 @@
-﻿namespace TankGame
+﻿
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TankGame
 {
     partial class Form1
     {
         /// <summary>
-        /// Required designer variable.
+        /// Обязательная переменная конструктора.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        /// Освободить все используемые ресурсы.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,21 +25,193 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Код, автоматически созданный конструктором форм Windows
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
         /// </summary>
+        /// 
+        List<WallObject> listwalls;
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "Form1";
+            this.Paint += PntMap;
+            listwalls = new List<WallObject>();
+            listwalls.Add(new WallObject(new Point(wallwidth, 0),new Point(wallwidth, this.Height- wallwidth*8)));
+            listwalls.Add(new WallObject(new Point(0, wallwidth),new Point(this.Width, wallwidth)));
+            listwalls.Add(new WallObject(new Point(this.Width- wallwidth*4, 0),new Point(this.Width- wallwidth*4, this.Height- wallwidth*8)));
+            listwalls.Add(new WallObject(new Point(0, this.Height - wallwidth * 8), new Point(this.Width, this.Height - wallwidth * 8)));
+
+
+
+
+
+
         }
 
+        private void PntMap(object sender, PaintEventArgs e)
+        {
+            foreach (var item in listwalls)
+            {
+                DrawObject(e, item.second, item.first);
+            }
+        }
+
+        GameHeroObject tank1 = new GameHeroObject();
+
+        int pacStep=5;
+        int wallwidth = 5;
+
+
+
+        private void DrawObject(PaintEventArgs e,Point second, Point first)
+        {
+            Pen pen1 = new Pen(Brushes.Black, wallwidth*2);
+            e.Graphics.DrawLine(pen1, first,second);
+        }
+
+        private bool CanRightMove()
+        {
+            bool maperror = false;
+            foreach (var item in listwalls)
+            {
+                if ((tank1.pos.Y + tank1.Height >= item.first.Y && tank1.pos.Y <= item.second.Y) || (item.first.Y == item.second.Y && item.first.Y + wallwidth > tank1.pos.Y && item.first.Y - wallwidth < tank1.pos.Y + tank1.Width))
+                {
+                    if (tank1.pos.X + tank1.Width <= item.first.X && (tank1.pos.X + tank1.Width + pacStep) >= item.first.X)
+                    {
+                        maperror = true;
+                        break;
+                    }
+                }
+
+            }
+            return maperror;
+           
+
+
+        }
+
+        private bool UpMove()
+        {
+            bool maperror = false;
+            foreach (var item in listwalls)
+            {
+                if ((tank1.pos.X + tank1.Width >= item.first.X && tank1.pos.X <= item.second.X))
+                {
+                    if ((tank1.pos.Y >= item.first.Y && (tank1.pos.Y - pacStep) <= item.first.Y))
+                    {
+                        maperror = true;
+                        break;
+                    }
+                }
+
+                if ((item.first.X == item.second.X && item.second.X + wallwidth > item.first.X && item.second.X - wallwidth < item.first.X + item.first.X))
+                {
+                    if ((tank1.pos.Y >= item.second.Y && (tank1.pos.Y - pacStep) <= item.second.Y))
+                    {
+                        maperror = true;
+                        break;
+                    }
+                }
+
+            }
+            return maperror;
+
+
+        }
+
+        private bool DownMove(object sender, PaintEventArgs e)
+        {
+            bool maperror = false;
+            foreach (var item in listwalls)
+            {
+                if ((tank1.pos.X + tank1.Width >= item.first.X && tank1.pos.X <= item.second.X))
+                {
+                    if ((tank1.pos.Y + tank1.Height <= item.first.Y && (tank1.pos.Y + tank1.Width + pacStep) >= item.first.Y))
+                    {
+                        maperror = true;
+                        break;
+                    }
+                }
+
+                if ((item.first.X == item.second.X && item.first.X - wallwidth > tank1.pos.X && item.first.X + wallwidth < tank1.pos.X + tank1.Width))
+                {
+                    if ((tank1.pos.Y + tank1.Height <= item.first.Y && (tank1.pos.Y + tank1.Height + pacStep) >= item.first.Y))
+                    {
+                        maperror = true;
+                        break;
+                    }
+                }
+
+            }
+            return maperror;
+        }
+
+
+        private bool LeftMove(object sender, PaintEventArgs e)
+        {
+            bool maperror = false;
+            foreach (var item in listwalls)
+            {
+                if ((tank1.pos.Y + tank1.Height >= item.first.Y && tank1.pos.Y <= item.second.Y) || (item.first.Y == item.second.Y && item.first.Y + 5 > tank1.pos.Y && item.first.Y - 5 < tank1.pos.Y + tank1.Height))
+                {
+                    if (tank1.pos.X >= item.second.X && (tank1.pos.X - pacStep) <= item.second.X)
+                    {
+                        maperror = true;
+                        break;
+                    }
+                }
+
+            }
+            return maperror;
+        }
+
+
+
         #endregion
+
+
+        class GameHeroObject
+        {
+            public Point pos { get; set; }
+            public int Width { get; set; }
+            public int Height { get; set; }
+
+            public GameHeroObject()
+            {
+                pos = new Point(1, 1);
+                Width = 10;
+                Height = 10;
+            }
+
+
+
+
+
+        }
+
+
+        class WallObject
+        {
+            public Point first;
+            public Point second;
+            public WallObject()
+            {
+                first = new Point();
+                second = new Point();
+            }
+
+            public WallObject(Point first, Point second)
+            {
+                this.first = new Point(first.X, first.Y);
+                this.second = new Point(second.X, second.Y);
+            }
+
+        }
     }
 }
 
