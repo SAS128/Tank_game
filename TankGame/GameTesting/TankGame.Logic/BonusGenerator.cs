8b86bf29af67fa705944cb470bcaacdfb6940bcf
Саша Шевченко <casiop8@gmail.com>
2020-03-30 17:18:19 +0300

    class BonusGenerator

---
+++ TankGame/GameTesting/TankGame.Logic/BonusGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TankGame.Logic
+{
+    class BonusGenerator : FieldObject
+    {
+        Bonus CurrentBonus;
+        int BonusRespawnInterval;
+        void SetNewRandomBonus()
+        {
+            CurrentBonus = new HPBonus();
+        }
+        public override object Collision(object sender)
+        {
+            object collision = null;
+            if (sender.GetType() == new Projectile().GetType())
+            {
+                collision = sender;
+            }
+            else if (sender.GetType() == new Tank().GetType())
+            {
+                collision = CurrentBonus.GiveEffect(sender);
+            }
+            return collision;
+        }
+    }
+}
