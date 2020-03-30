b817c1aa178faa16b8785bd5443342e32e427c72
Александр Захаров <alexz20937cany3@gmail.com>
2020-03-30 20:41:53 +0300

    StatisticForm with LocalDB

--- TankGame/TankGame/StatisticForm.Designer.cs
+++ TankGame/TankGame/StatisticForm.Designer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.SQLite;
 using System.Drawing;
 using System.IO;
@@ -49,13 +50,59 @@ namespace TankGame
 
             AllGames.Location = new Point(130, 80);
             AllGames.Size = new Size(400, 300);
-            AllGames.Items.Add((AllGames.Items.Count + 1).ToString() + ") Random1 Vs Random2 WIN GameTime: 00:12:24");
-            AllGames.Items.Add((AllGames.Items.Count + 1).ToString() + ") Random1 Vs Random4 LOSE GameTime: 00:5:13");
-
+            //AllGames.Items.Add((AllGames.Items.Count + 1).ToString() + ") Random1 Vs Random2 WIN GameTime: 00:12:24");
+            //AllGames.Items.Add((AllGames.Items.Count + 1).ToString() + ") Random1 Vs Random4 LOSE GameTime: 00:5:13");
+            this.Load += ShowStatistic;
 
             this.Controls.Add(UserName);
             this.Controls.Add(Win_Lose_Count);
             this.Controls.Add(AllGames);
+        }
+        string CurrentUser ="User1";
+        private void ShowStatistic(object sender, EventArgs e)
+        {
+            int k = ExistUserLocal("User1");
+            
+            
+            List<StatisticUser> userStat = new List<StatisticUser>();
+            if (k>0)
+            {
+                using (SQLiteConnection conn = new SQLiteConnection(string.Format("Data Source={0};", databaseName)))
+                {
+                    conn.Open();
+                    using (SQLiteCommand command = new SQLiteCommand($"select * from StatisticTable where [UserIdStatictic]={k}", conn))
+                    {
+                        SQLiteDataReader reader = command.ExecuteReader();
+                        while(reader.Read())
+                        {
+                            userStat.Add(new StatisticUser(reader.GetString(1), DateTime.Parse(reader.GetString(3)).TimeOfDay, reader.GetBoolean(2)));
+                        }
+
+                        reader.Close();
+                        //Добавить синхронизацию с классами  
+                        conn.Close();
+                    }
+                }
+
+            }
+            else
+            {
+                //информация с сервера
+            }
+            int tmp = 0;
+            foreach(var item in userStat)
+            {
+                AllGames.Items.Add((AllGames.Items.Count + 1).ToString() +$") {CurrentUser} Vs {item.NickNameEnemy} "+ ((item.IsWin)?"WIN":"LOSE")+$" GameTime: {item.MatchLength}");
+                if (item.IsWin == true)
+                {
+                    tmp++;
+
+                }
+            }
+            Win_Lose_Count.Text = tmp + "/"+userStat.Count+"  "+Convert.ToInt32(100.0/ Convert.ToDouble(userStat.Count)*Convert.ToDouble(tmp))+"%";
+
+
+
         }
 
         Label UserName = new Label();
@@ -64,13 +111,41 @@ namespace TankGame
 
         public static string databaseName = @"Local_User_DB_Tanks.sqlite";
      
-        private static void InsertData_to_StatisticTable(string NickName, bool IsWin, TimeSpan time)
+
+        
+        public static int ExistUserLocal(string Name)
         {
+            int isInDB= -1;
+
+            using (SQLiteConnection conn = new SQLiteConnection(string.Format("Data Source={0};", databaseName)))
+            {
+                conn.Open();
+                using (SQLiteCommand comand = new SQLiteCommand($"select ID from LocalUserTable WHERE [Login]='{Name}'", conn))
+                {
+                    SQLiteDataReader reader = comand.ExecuteReader();
+                    reader.Read();
+                    if (reader.FieldCount>0)
+                    {
+                        
+                        isInDB = reader.GetInt32(0);
+                    }
+
+                    reader.Close();
+                    //Добавить синхронизацию с классами  
+                    conn.Close();
+                }
+            }
+            return isInDB;
+        }
+
+        private static void InsertData_to_StatisticTable(string NickName, bool IsWin, TimeSpan time,int ID)
+        {
+            
             using (SQLiteConnection conn = new SQLiteConnection(string.Format("Data Source={0};", databaseName)))
             {
                 conn.Open();
                 using (SQLiteCommand command = new SQLiteCommand($"INSERT INTO StatisticTable (" +
-                    $"[NickNameEnemyPlayer],[IsWin],[LenghBattle]) VALUES ('{NickName}',{IsWin},'{time.ToString()}');", conn))//Заменить на передаваемые данные
+                    $"[NickNameEnemyPlayer],[IsWin],[LenghBattle],[UserIdStatictic]) VALUES ('{NickName}',{IsWin},'{time.ToString()}',{ID});", conn))//Заменить на передаваемые данные
                 {
                     command.ExecuteNonQuery();
                     conn.Close();
@@ -78,19 +153,7 @@ namespace TankGame
 
             }
         }
-        private static void ShowStatistic()
-        {
-            using (SQLiteConnection conn = new SQLiteConnection(string.Format("Data Source={0};", databaseName)))
-            {
-                conn.Open();
-                using (SQLiteCommand command = new SQLiteCommand("select NickNameEnemyPlayer from StatisticTable", conn))
-                {
-                    SQLiteDataReader reader = command.ExecuteReader();
-                    //Добавить синхронизацию с классами  
-                    conn.Close();
-                }
-            }
-        }
+       
         /// <summary>
         /// LocalUserTable
         /// </summary>
---
+++ TankGame/TankGame/StatisticUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TankGame
+{
+    class StatisticUser
+    {
+        public string NickNameEnemy;
+        public TimeSpan MatchLength;
+        public bool IsWin;
+        public StatisticUser()
+        {
+            NickNameEnemy = String.Empty;
+            MatchLength = new TimeSpan();
+            IsWin = false;
+        }
+
+        public StatisticUser(string NickNameEnemy,TimeSpan MatchLength,bool IsWin)
+        {
+            this.NickNameEnemy = NickNameEnemy;
+            this.MatchLength = MatchLength;
+            this.IsWin = IsWin;
+        }
+    }
+}
